4dacc9a57458c919a86e7d83766fee9e2465fdc6
prabhu112317 <prabhu112317@gmail.com>
2025-10-18 15:33:47 +0530

    Add files via upload

---
+++ basic 10/Q3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+ {
+    int num;
+    int count = 0;
+    
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    if (num < 0)
+        num = -num;
+
+    if (num == 0)
+        count = 1;
+        
+    else 
+	    {
+        while (num != 0) {
+            num = num / 10;  
+            count++;
+        }
+    }
+
+    printf("Number of digits = %d\n", count);
+
+    return 0;
+}
